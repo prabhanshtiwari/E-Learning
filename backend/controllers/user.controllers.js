be064e19536687b797f959cad16701f55e635429
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2026-06-16 10:26:15 +0530

    Creating register api and email is sent successfully

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,7 +1,52 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import sendMail from "../middlewares/sendMail.middleware.js";
+
 export const register = async (req, res) => {
   try {
+    const { email, name, password } = req.body;
 
-    res.send("Register api")
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    const activationToken = jwt.sign(
+      {
+        newUser: { name, email, password: hashedPassword },
+        otp,
+      },
+      process.env.ACTIVATION_SECRET,
+      {
+        expiresIn: "5m",
+      },
+    );
+
+    const data = {
+      name,
+      otp,
+    };
+
+    await sendMail(email, "E learning Verification", data);
+
+    res.status(200).json({
+      message: "OTP sent to your email address",
+      activationToken, // Sent to client to pass into /verify endpoint next
+    });
   } catch (error) {
     res.status(500).json({
       message: error.message,
--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
-import express from 'express';
-import  dotenv from "dotenv";
-import { connectDB } from './db/index.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
 
 dotenv.config();
 
 const app = express();
 
+// Using middlewares
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send({
-        message: "Hi from index.js backend",
-    })
-})
+  res.send({
+    message: "Hi from index.js backend",
+  });
+});
 
 // Routes import
 import userRoutes from "./routes/user.routes.js";
 
 // Using routes
-app.use("/api/user", userRoutes); 
-
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+});
