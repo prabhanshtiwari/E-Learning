ba39debfbb1f194739f274086036739aa1be9ccd
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2026-06-16 11:15:14 +0530

    verify user controller done

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -2,54 +2,77 @@ import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import sendMail from "../middlewares/sendMail.middleware.js";
+import TryCatch from "../middlewares/TryCatch.js";
 
-export const register = async (req, res) => {
-  try {
-    const { email, name, password } = req.body;
+export const register = TryCatch(async (req, res) => {
+  const { email, name, password } = req.body;
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (user) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    user = {
-      name,
-      email,
-      password: hashedPassword,
-    };
-
-    const otp = Math.floor(100000 + Math.random() * 900000);
-
-    const activationToken = jwt.sign(
-      {
-        newUser: { name, email, password: hashedPassword },
-        otp,
-      },
-      process.env.ACTIVATION_SECRET,
-      {
-        expiresIn: "5m",
-      },
-    );
-
-    const data = {
-      name,
-      otp,
-    };
-
-    await sendMail(email, "E learning Verification", data);
-
-    res.status(200).json({
-      message: "OTP sent to your email address",
-      activationToken, // Sent to client to pass into /verify endpoint next
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
+  if (user) {
+    return res.status(400).json({
+      message: "User already exists",
     });
   }
-};
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  user = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  const otp = Math.floor(100000 + Math.random() * 900000);
+
+  const activationToken = jwt.sign(
+    {
+      user: { name, email, password: hashedPassword },
+      otp,
+    },
+    process.env.ACTIVATION_SECRET,
+    {
+      expiresIn: "5m",
+    },
+  );
+
+  const data = {
+    name,
+    otp,
+  };
+
+  await sendMail(email, "E learning Verification", data);
+
+  res.status(200).json({
+    message: "OTP sent to your email address",
+    activationToken, // Sent to client to pass into /verify endpoint next
+  });
+});
+
+export const verifyUser = TryCatch(async (req, res) => {
+  const { otp, activationToken } = req.body;
+
+  const verify = jwt.verify(activationToken, process.env.ACTIVATION_SECRET);
+
+  if (!verify) {
+    return res.status(400).json({
+      message: "OTP Expired",
+    });
+  }
+
+  if (verify.otp !== otp) {
+    return res.status(400).json({
+      message: "Wrong OTP",
+    });
+  }
+
+  await User.create({
+    name: verify.user.name,
+    email: verify.user.email,
+    password: verify.user.password,
+  });
+
+  res.json({
+    message: "User Registered",
+  });
+});
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/user.controllers.js";
+import { register, verifyUser } from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/verify", verifyUser);
 
 export default router;
