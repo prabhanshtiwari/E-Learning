b6d6144a7d0ae5f2a9b3c5538cded9f2ea270911
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2026-06-17 16:04:50 +0530

    Stated frontend: build header component

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 import Razorpay from "razorpay";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const app = express();
 
 // Using middlewares
 app.use(express.json());
+app.use(cors());
 
 app.use("/uploads", express.static("uploads"));
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Header from "./components/header/Header.jsx";
+
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
