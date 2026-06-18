38933b8bd3c247ad4c0c8b81dbb05faf36f1403e
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2026-06-18 10:37:50 +0530

    updated footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
     <footer>
       <div className="footer-content">
         <p>
-          &copy; 2024 Your E-Learning Platform. All rights reserved. <br /> Made
-          with ❤️ <a href="">Prashant Kumar</a>
+          &copy; 2026 Your E-Learning Platform. All rights reserved. <br /> Made
+          with ❤️ <a href="">Prabhansh Tiwari</a>
         </p>
         <div className="social-links">
           <a href="">
